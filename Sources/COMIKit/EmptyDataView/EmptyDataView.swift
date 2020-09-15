698f7c5f9627656bbfbb0cac45ed4eedc79eb1af
COMI <admin@wincomi.com>
2020-09-15 17:37:05 +0800

    Add EmptyDataView initialization

--- Sources/COMIKit/EmptyDataView/EmptyDataView.swift
+++ Sources/COMIKit/EmptyDataView/EmptyDataView.swift
@@ -11,6 +11,13 @@ public struct EmptyDataView: View {
 	public var buttonTitle: String?
 	public var buttonAction: (() -> Void)?
 
+	public init(title: String, description: String? = nil, buttonTitle: String? = nil, buttonAction: (() -> Void)? = nil) {
+		self.title = title
+		self.description = description
+		self.buttonTitle = buttonTitle
+		self.buttonAction = buttonAction
+	}
+
 	public var body: some View {
 		VStack(alignment: .center, spacing: 12) {
 			Text(title)
