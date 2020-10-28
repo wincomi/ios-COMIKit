1e42aaeaa5415b0b4eae3034f70926b0a9a199c8
COMI <admin@wincomi.com>
2020-10-28 17:51:21 +0800

    Fix present(contextMenuItems:animated:configurePopoverPresentationController:)

--- Sources/COMIKit/ContextMenu/UIViewController+ContextMenu.swift
+++ Sources/COMIKit/ContextMenu/UIViewController+ContextMenu.swift
@@ -11,10 +11,14 @@ public extension UIViewController {
 	}
 
 	func present(contextMenuItems: [ContextMenuItem], animated: Bool, configurePopoverPresentationController: ((UIPopoverPresentationController) -> Void)? = nil) {
-		let alert = UIAlertController(contextMenuItems: contextMenuItems, childrenHandler: present(contextMenuItems:))
+		let alert = UIAlertController(contextMenuItems: contextMenuItems) { [weak self] contextMenuItems in
+			self?.present(contextMenuItems: contextMenuItems, animated: animated, configurePopoverPresentationController: configurePopoverPresentationController)
+		}
+
 		if let popoverPresentationController = alert.popoverPresentationController {
 			configurePopoverPresentationController?(popoverPresentationController)
 		}
+
 		present(alert, animated: animated)
 	}
 }
